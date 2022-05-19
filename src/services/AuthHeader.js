2b3d71e686a5ae5e698d59e84a824f61b83cc8ad
Zhaosi Qu <git@chaos.run>
2022-05-20 00:00:26 +0300

    Remove unused service folder

--- src/services/AuthHeader.js
+++
@@ -1,8 +0,0 @@
-export default function AuthHeader() {
-  let user = JSON.parse(localStorage.getItem('user'));
-  if (user && user.token ){
-    return { Authorization: 'Basic' + user.token };
-  } else {
-    return {};
-  }
-}
