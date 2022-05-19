8c452e78567605e4f337198e89e14bcaa8620721
Zhaosi Qu <git@chaos.run>
2022-05-20 02:04:56 +0300

    Simplify auth part

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,93 +2,55 @@
   <div class="grid">
     <vs-row justify="center" align="center">
       <vs-col w="6">
-          <div class="card mt-5">
-            <div class="card-header">
-              Sign In
-            </div>
-            <div class="card-body mt-5">
-              <form @submit="handleAuthentication">
-                <div class="mb-3">
-                  <label for="email" class="form-label">Email address</label>
-                  <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
-                  <div id="usernameHelp" class="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div class="mb-3">
-                  <label for="password" class="form-label">Password</label>
-                  <input type="password" class="form-control" id="password">
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-              </form>
-            </div>
+        <div class="card mt-5">
+          <div class="card-header">Sign In</div>
+          <div class="card-body mt-5">
+            <form>
+              <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="email"
+                  aria-describedby="emailHelp"
+                  v-model="email"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="password"
+                  v-model="password"
+                />
+              </div>
+              <a class="btn btn-primary" @click="login">Log in</a>
+            </form>
           </div>
+        </div>
       </vs-col>
     </vs-row>
   </div>
 </template>
 
 <script>
-  import axios from 'axios';
-  export default {
-    name: "authenticate-user",
-    
-    data(){
-      return {
-        user: {
-          id: "",
-          email: "",
-          role: "",
-          name: "",
-          library: "",
-          token: "",
-          authenticated: false
-        }
-      }
-    },
+import auth from "./auth";
 
-    computed: {
-      authenticated(){
-        return this.user.authenticated;
-      }
-    },
-    
-    created() {
-      if(this.user.authenticated){
-        this.$router.push({path: '/dashboard'})
-      }
-    },
+export default {
+  name: "Login",
 
-    methods: {
-      handleAuthentication(e){
-        e.preventDefault();
-        const email = e.target.email.value;
-        const password =  e.target.password.value;
-        const token = btoa(email+":"+password);
-        axios.get(`${process.env.VUE_APP_API_BASE_URL}/auth`, { 
-            headers: { 'Authorization': 'Basic '+ token }
-          })
-        
-        .then(response => {
-          this.user.id = response.data.id;
-          this.user.name = response.data.name;
-          this.user.email = response.data.email;
-          this.user.role = response.data.role;
-          this.user.library = response.data.library;
-          this.user.authenticated = true;
-          this.user.token = token;
-          window.localStorage.setItem('user', JSON.stringify(this.user));
-          window.localStorage.setItem('user-token', token);
-          window.localStorage.setItem("username", this.user.name);
-          if(this.user.role === process.env.VUE_APP_LIBRARIAN_ROLE){
-            this.$router.push({name: 'admin-dashboard'})
-          }else{
-            this.$router.push({name: 'user-dashboard'})
-          }
-        })
-        .catch(error => {
-          console.log(error);
-        });
+  data: function () {
+    return {
+      email: "",
+      password: "",
+    };
+  },
 
-      }
-    }
-  }
+  methods: {
+    login: function () {
+      auth.login(this, this.email, this.password);
+    },
+  },
+};
 </script>
